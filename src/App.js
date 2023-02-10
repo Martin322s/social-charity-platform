d5e69636d05fa23a1adb222eb409e4f026374a49
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-02-10 16:09:23 +0200

    Home Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./components/common/Footer/Footer";
 import Navigation from "./components/common/Header/Navigation";
+import Home from "./components/Home/Home";
 
 function App() {
     return (
         <>
             <Navigation />
+                <Home />
             <Footer />
         </>
     );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import "./styles/home.css";
+
+const Home = () => {
+    return (
+        <main>
+            <h1>Welcome to WINK -</h1>
+            <h3>Social Charity Platform</h3>
+            <img className="main-img" src="./images/favicon.png" alt="mainImg" />
+        </main>
+    );
+}
+
+export default Home;
