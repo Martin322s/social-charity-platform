6eadfabf7d7f641d09854526829b4f160f69f276
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-02-11 10:22:27 +0200

    fix problems

--- src/components/Not Found/Error.js
+++ src/components/Not Found/Error.js
@@ -1,4 +1,3 @@
-import logo from "./images/404.png";
 import "./styles/error.css";
 
 const Error = () => {
@@ -6,9 +5,6 @@ const Error = () => {
         <main className="error-container">
             <h1 className="error-heading">Error 404</h1>
             <h3 className="error-heading-three">Page not found!</h3>
-            <div className="error-img">
-                <img src={logo} alt="not found" />
-            </div>
         </main>
     );
 }
