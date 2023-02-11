d066f3321506ee27448f34f015817b83f472a3d3
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-02-11 12:54:25 +0200

    create publication

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,9 +1,39 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../../contexts/userContext";
+import { createPublication } from "../../services/publicationService";
 import "./styles/create.css";
 
 const Create = () => {
+    const { user } = useContext(AuthContext);
+    const [data, setData] = useState({
+        title: "",
+        category: "",
+        content: ""
+    });
+
+    const changeHandler = (ev) => {
+        setData(state => ({
+            ...state,
+            [ev.target.name]: ev.target.value
+        }));
+    }
+
+    const submitHandler = (ev, data, token, id) => {
+        ev.preventDefault();
+
+        if (data.title === '' || data.category === '' || data.content === '') {
+            alert("All fields are required!");
+        } else {
+            createPublication({ ...data, _ownerId: id }, token)
+                .then(data => {
+                    console.log(data);
+                });
+        }
+    }
+
     return (
         <main>
-            <form className="register">
+            <form className="register" onSubmit={(ev) => submitHandler(ev, data, user.accessToken, user._id)}>
                 <h3 className="create-heading">
                     <i className="fas fa-plus-square" />
                     Create Publication
@@ -11,11 +41,25 @@ const Create = () => {
                 <label className="label" htmlFor="title">
                     Title:
                 </label>
-                <input className="inputField" type="text" id="title" name="title" />
+                <input
+                    className="inputField"
+                    type="text"
+                    id="title"
+                    name="title"
+                    value={data.title}
+                    onChange={(ev) => changeHandler(ev)}
+                />
                 <label className="label" htmlFor="category">
                     Category:
                 </label>
-                <input className="inputField" type="text" id="category" name="category" />
+                <input
+                    className="inputField"
+                    type="text"
+                    id="category"
+                    name="category"
+                    value={data.category}
+                    onChange={(ev) => changeHandler(ev)}
+                />
                 <label className="label" htmlFor="content">
                     Content:
                 </label>
@@ -24,7 +68,8 @@ const Create = () => {
                     id="content"
                     cols={40}
                     rows={10}
-                    defaultValue={""}
+                    value={data.content}
+                    onChange={(ev) => changeHandler(ev)}
                 />
                 <button className="button">Create</button>
             </form>
---
+++ src/services/publicationService.js
@@ -0,0 +1,15 @@
+const baseUrl = "http://localhost:3030/publications";
+
+export const createPublication = (userData, token) => {
+    return fetch(`${baseUrl}/create`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Authorization': token
+        },
+        body: JSON.stringify(userData)
+    })
+        .then(res => res.json());
+}
+
+export const getAll = () => fetch(`${baseUrl}/`)
