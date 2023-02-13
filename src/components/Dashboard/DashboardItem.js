f56bf266a08ace1c19db0ec5e52f1ef70f418f81
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-02-13 16:48:51 +0200

    show details if user is logged in

--- src/components/Dashboard/DashboardItem.js
+++ src/components/Dashboard/DashboardItem.js
@@ -1,14 +1,20 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/userContext";
 import "./styles/dashboard.css";
 
 const DashboardItem = ({ publicationData }) => {
+    const { user } = useContext(AuthContext);
     return (
         <article className="publication">
             <h1>{publicationData.title}</h1>
             <span className="content-cat ">{publicationData.category}</span>
-            <div className="activities">
-                <Link className="btn details-btn" to={`/details/${publicationData._id}`}>Details</Link>
-            </div>
+            {user.accessToken ?
+                <div className="activities">
+                    <Link className="btn details-btn" to={`/details/${publicationData._id}`}>Details</Link>
+                </div>
+                : null
+            }
         </article>
     );
 }
