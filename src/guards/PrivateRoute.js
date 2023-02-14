c6983a8818bd546f03a6e810d28b1b6a8d5534a1
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-02-14 12:27:01 +0200

    change paths

--- src/guards/PrivateRoute.js
+++ src/guards/PrivateRoute.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import { AuthContext } from "../contexts/AuthContext";
+import { AuthContext } from "../contexts/userContext";
 
 const PrivateRoute = ({ children }) => {
     const { user } = useContext(AuthContext);
