f813f3a2a159082b354247bfb9a7cf9744404d01
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-04-24 22:36:12 +0300

    import delete with lazy

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Footer from "./components/common/Footer/Footer";
 import Navigation from "./components/common/Header/Navigation";
 import Create from "./components/Create/Create";
 import Dashboard from "./components/Dashboard/Dashboard";
-import Delete from "./components/Delete/Delete";
-import Details from "./components/Details/Details";
-import Edit from "./components/Edit/Edit";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
-import Logout from "./components/Logout/Logout";
-import Error from "./components/Not Found/Error";
 import Profile from "./components/Profile/Profile";
 import Register from "./components/Register/Register";
 import { AuthProvider } from "./contexts/userContext";
 import PrivateRoute from "./guards/PrivateRoute";
 import RouteGuard from "./guards/RouteGuard";
+
+const Delete = React.lazy(() => import("./components/Delete/Delete"));
+import Details from "./components/Details/Details";
+import Edit from "./components/Edit/Edit";
+import Logout from "./components/Logout/Logout";
+import Error from "./components/Not Found/Error";
+import React from "react";
 
 function App() {
     return (
